d366f622f9c8c9c68e50c7d0f94e2fd3a5ed65c4
kleber20242 <gudezinn@gmail.com>
2025-10-20 16:23:11 -0300

    versao final

---
+++ index.js
@@ -0,0 +1,12 @@
+function sumir() {
+  var qrcode = document.getElementById("qrcode");
+  var botao = document.getElementById("btn");  
+  var section = document.getElementById("section");
+
+  if (qrcode.style.display = "none") {
+    qrcode.style.display = "grid";
+    section.style.display = "none";
+
+  
+}
+}
